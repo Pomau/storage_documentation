752ee8e06a69dcaef268d915f98f0dcb15382215
biktimirov.timur <bikt38@gmail.com>
2024-12-09 13:08:28 +0300

    add ssl

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,19 +4,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0',
+    host: true,
     port: 5173,
-    proxy: {
-      '/api': {
-        target: process.env.VITE_API_URL || 'http://localhost:8080',
-        changeOrigin: true,
-        secure: false,
-      }
+    watch: {
+      usePolling: true
     }
-  },
-  base: '/',
-  preview: {
-    port: 5173,
-    strictPort: true,
   }
 }) 

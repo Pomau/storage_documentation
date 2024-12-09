5dde99628faba3327ffaa9b3d087f56f2d1a6061
biktimirov.timur <bikt38@gmail.com>
2024-12-09 14:05:59 +0300

    docker

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { message } from 'antd';
 
 export const api = axios.create({
-    baseURL: 'http://localhost:8080/api',
+    baseURL: 'https://pomau.ru/api',
     headers: {
         'Content-Type': 'application/json',
     },
--- frontend/src/api/client.ts
+++ frontend/src/api/client.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 export const apiClient = axios.create({
-    baseURL: 'http://localhost:8080/api',
-    headers: {
-        'Content-Type': 'application/json',
-    },
-}); 
+	baseURL: 'https://pomau.ru/api',
+	headers: {
+		'Content-Type': 'application/json',
+	},
+})
